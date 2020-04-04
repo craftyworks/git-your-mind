72627812192adf15905592fadb4126aae455555c
craftyworks <craftyworks@gmail.com>
2020-04-04 20:58:33 +0900

    :tada: first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import { BootstrapVue } from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.use(BootstrapVue)
+
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+dom.watch()
+
+Vue.config.productionTip = false
 new Vue({
   render: h => h(App)
 }).$mount('#app')
