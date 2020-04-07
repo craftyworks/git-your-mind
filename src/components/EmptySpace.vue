072407bec4f42396b65557e8ccde649574049bc7
DDam40 <craftyworks@gmail.com>
2020-04-07 22:42:05 +0900

    Create EmptySpace.vue

---
+++ src/components/EmptySpace.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="empty-space" :style="emptyStyle">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptySpace',
+  props: {
+    count: {
+      type: Number,
+      default: 1
+    }
+  },
+  computed: {
+    emptyStyle () {
+      return {
+        height: `${this.count * 90}px`
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .empty-space {
+    width: 60px;
+    display: flex;
+    justify-content: center;
+  }
+</style>
