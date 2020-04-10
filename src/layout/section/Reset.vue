5fd698d8583c64067ccc7a144aba4a32edac5051
DDam40 <craftyworks@gmail.com>
2020-04-10 18:48:16 +0900

    Update Reset.vue

--- src/layout/section/Reset.vue
+++ src/layout/section/Reset.vue
@@ -1,22 +1,22 @@
 <template>
   <!-- Section-->
   <section class="page-section" id="reset">
-    <div class="container">
-      <!-- Section Heading-->
-      <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Reset</h2>
-      <!-- Icon Divider-->
-      <div class="divider-custom">
-        <div class="divider-custom-line"></div>
-        <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-        <div class="divider-custom-line"></div>
-      </div>
-      <!-- Soft Reset -->
-      <reset-soft></reset-soft>
-      <!-- Icon Divider-->
-      <div class="divider-custom"><div class="divider-custom-line"></div></div>
-      <!-- Hard Reset -->
-      <reset-hard></reset-hard>
+    <!-- Section Heading-->
+    <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Reset</h2>
+    <!-- Icon Divider-->
+    <div class="divider-custom">
+      <div class="divider-custom-line"></div>
+      <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+      <div class="divider-custom-line"></div>
     </div>
+    <!-- Soft Reset -->
+    <reset-soft></reset-soft>
+    <!-- Icon Divider-->
+    <div class="divider-custom">
+      <div class="divider-custom-line"></div>
+    </div>
+    <!-- Hard Reset -->
+    <reset-hard></reset-hard>
   </section>
 </template>
 
